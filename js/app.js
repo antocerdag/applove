8e72e666814735661e96ab3028de9483e3a0b234
Antonia Cerda <antoniacerdag@gmail.com>
2017-06-01 11:30:58 -0400

    arreglando modal

--- js/app.js
+++ js/app.js
@@ -26,31 +26,29 @@
 		});		
 	});
 })();
-/*modal-body ---> bodyDiv 
-modal ----> div
-boxes ---> col4*/
-(function imgModal(){
-	var col4 = Array.from(document.getElementsByClassName("col-4"));
-	var div = document.getElementById("imgproductos");
-	var bodyDiv, close,img;
 
-	col4.forEach(function(col){
-		col.addEventListener("click", function(){
-			div.innerHTML = "";
-			bodyDiv = document.createElement("div");
-			bodyDiv.classList.add("body-div");
-			bodyDiv.innerHTML = col.innerHTML;
-			div.appendChild(bodyDiv);
-			div.classList.remove("hide");
+(function imgModal(){
+	var img = Array.from(document.getElementsByClassName("image"));
+	var modal = document.getElementById("imgproductos");
+	var bodyModal, close, img;
+
+	img.forEach(function(foto){
+		foto.addEventListener("click", function(){
+			modal.innerHTML = "";
+			bodyModal = document.createElement("div");
+			bodyModal.classList.add("modal-body");
+			bodyModal.innerHTML = foto.innerHTML;			
+			modal.appendChild(bodyModal);
+			modal.classList.remove("hide");
 			close = document.createElement("div");
 			close.classList.add("close");
 			img = document.createElement("img");
 			img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
 			close.appendChild(img);
-			div.appendChild(close);
-			close.addEventListener("click", function(){
-				div.classList.add("hide");
-			})
-		})
-	})
-})
+			modal.appendChild(close);
+			close.addEventListener("click",function(){
+				modal.classList.add("hide");
+			});
+		});		
+	});
+})();
